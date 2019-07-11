8b045ff3904a3fcd761ad1069192cc0774c77457
tsemach <tsemach.mizrachi>
2019-07-11 23:35:28 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>    
+        <h1>React Applicaiton Using Parcel</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
